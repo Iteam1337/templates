514917c430db9a157d9901fc4b88a4cba8ee501d
Erik Hellman <erik.hellman@iteam.se>
2022-03-30 13:57:58 +0200

    Iteamify react templates

--- templates/react-ts/src/App.tsx
+++ templates/react-ts/src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <header className="w-full max-w-md flex flex-col items-center">
-        <img className="animate-spin-slow" src={logo} alt="logo" />
-        <h1 className="text-2xl font-bold">Hello Vite + React!</h1>
+        <h1 className="text-2xl font-bold">Hello Iteam + React + Typescript!</h1>
+        <img src="https://cdn-assets-cloud.frontify.com/local/frontify/eyJwYXRoIjoiXC9wdWJsaWNcL3VwbG9hZFwvc2NyZWVuc1wvMTUzOTg2XC9lZWNmNjExMDg1YTI1YWM0MTIzZGE3NmY4M2EzZTdkNi0xNTEyNzI5Mzk3LnBuZyJ9:frontify:ANleqg1_q50wrm-EmaePRoqAtTkpInN90J70KGuqUbU?width=449" className="App-logo" alt="logo" />
       </header>
     </div>
   );
--- templates/react/src/App.jsx
+++ templates/react/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
@@ -8,35 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <h2>Hello Iteam + React!</h2>
+        <img src="https://cdn-assets-cloud.frontify.com/local/frontify/eyJwYXRoIjoiXC9wdWJsaWNcL3VwbG9hZFwvc2NyZWVuc1wvMTUzOTg2XC9lZWNmNjExMDg1YTI1YWM0MTIzZGE3NmY4M2EzZTdkNi0xNTEyNzI5Mzk3LnBuZyJ9:frontify:ANleqg1_q50wrm-EmaePRoqAtTkpInN90J70KGuqUbU?width=449" className="App-logo" alt="logo" />
       </header>
     </div>
   )
