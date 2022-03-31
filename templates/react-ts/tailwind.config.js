fc68934059de4b85dd315e9df5db8f2e2c14c2d2
Andreas Lundqvist <momentiris@gmail.com>
2022-03-31 11:42:04 +0200

    chore(react, react-ts): remove unused spinner animation

--- templates/react-ts/tailwind.config.js
+++ templates/react-ts/tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      animation: {
-        'spin-slow': 'spin 5s linear infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
