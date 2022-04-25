c95d9e0be4c1fac30541fb3649fcdcdb0cf4d4d6
Emil Ahlbäck <e.ahlback@gmail.com>
2022-04-25 14:03:21 +0200

    feat(templates): koa.js typescript template
    * koa-ts template first draft
    
    * Add koa-ts to templates, rename others for clarity
    
    * Add .prettierrc
    
    * Add node 16 to workflow matrix
    
    * add .vscode directory including extensions.json and settings.json
    
    * remove tailwindcss from extensions.json
    
    Co-authored-by: @mittistormen <johanna.grahn@iteam.se>

---
+++ templates/koa-ts/src/app.ts
@@ -0,0 +1,29 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import pinoLogger from 'koa-pino-logger';
+
+import exampleApi from './example-api';
+import errorHandler from './middlewares/error-handler';
+
+const app = new Koa();
+const logger = pinoLogger();
+
+app.use(logger);
+
+app.on('error', (err) => {
+  console.log('on:error');
+  logger.logger.error(err);
+});
+
+app.use(errorHandler());
+
+// TODO: Remove me. koa-pino-logger uses standard log levels
+app.use(async (ctx, next) => {
+  ctx.log.warn('Hello');
+  await next();
+});
+
+app.use(bodyParser());
+app.use(exampleApi.routes());
+
+export default app;
