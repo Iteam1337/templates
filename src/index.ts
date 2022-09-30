95c761e1c2f1efe81f6c2ffd024485ac149a9be9
Andreas Lundqvist <momentiris@gmail.com>
2022-09-30 17:06:34 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,11 @@ import * as types from './types';
 import * as prompts from './prompts';
 import * as utils from './utils';
 
-const handleCommand = (command: types.Command) => {
-  const commandHandlers: Record<types.Command, () => Promise<void>> = {
-    add: prompts.add,
-    create: prompts.create,
-  };
-
-  return commandHandlers[command]();
+const commandHandlers: Record<types.Command, () => Promise<void>> = {
+  add: prompts.add,
+  create: prompts.create,
 };
+
+const handleCommand = (command: types.Command) => commandHandlers[command]();
 
 prompts.start().then(handleCommand).catch(utils.handleError);
