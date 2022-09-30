40f5e15885fa54c2bcaf28e3e01c280e6ec439e7
Andreas Lundqvist <momentiris@gmail.com>
2022-09-30 16:58:56 +0200

    clean up

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,22 +1,22 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
 export const copyDir = (srcDir: string, destDir: string) => {
-  fs.mkdirSync(destDir, { recursive: true })
+  fs.mkdirSync(destDir, { recursive: true });
 
   const prepareAndCopy = (file: string) => {
-    const srcFile = path.resolve(srcDir, file)
-    const destFile = path.resolve(destDir, file)
-    copy(srcFile, destFile)
-  }
+    const srcFile = path.resolve(srcDir, file);
+    const destFile = path.resolve(destDir, file);
+    copy(srcFile, destFile);
+  };
 
-  fs.readdirSync(srcDir).forEach(prepareAndCopy)
-}
+  fs.readdirSync(srcDir).forEach(prepareAndCopy);
+};
 
 export const write = (to: string, content: string) =>
-  fs.writeFileSync(to, content)
+  fs.writeFileSync(to, content);
 
 export const copy = (src: string, dest: string) =>
   fs.statSync(src).isDirectory()
     ? copyDir(src, dest)
-    : fs.copyFileSync(src, dest)
+    : fs.copyFileSync(src, dest);
