061ccb6bae254877853ab2690e46352b9f217d63
Emil Ahlbäck <emil.ahlback@iteam.se>
2022-12-16 15:29:37 +0100

    feat: koa-ts uses process.env.PORT if available

--- templates/koa-ts/src/index.ts
+++ templates/koa-ts/src/index.ts
@@ -1,5 +1,6 @@
 import app from './app';
 
-app.listen(3000, () => {
-  console.log(`listening on http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
 });
